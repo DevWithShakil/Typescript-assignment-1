4bbb502917dad5601c9fd48b8ceab9703dcdfb16
Shakil Khan <m.shakilkhan720@gmail.com>
2023-05-08 00:44:24 +0600

    problem-5 has been solved

--- Problems/problem_4.ts
+++ Problems/problem_4.ts
@@ -1,32 +1,32 @@
-class Person {
-  constructor(
-    private name: string, 
-    private age: number
-    ){}
+// class Person {
+//   constructor(
+//     private name: string, 
+//     private age: number
+//     ){}
 
-  public getDetails(): string {
-    return `Name: ${this.name}, 
-            Age: ${this.age}`;
-  }
-}
+//   public getDetails(): string {
+//     return `Name: ${this.name}, 
+//             Age: ${this.age}`;
+//   }
+// }
 
-class Student extends Person {
-  constructor(
-    name: string, 
-    age: number, 
-    private grade: string) 
-{
-    super(name, age);
-  }
+// class Student extends Person {
+//   constructor(
+//     name: string, 
+//     age: number, 
+//     private grade: string) 
+// {
+//     super(name, age);
+//   }
 
-  public getGrade(): string {
-    return `Grade: ${this.grade}`;
-  }
-}
+//   public getGrade(): string {
+//     return `Grade: ${this.grade}`;
+//   }
+// }
 
-const person = new Person('Shakil Khan', 25);
-console.log(person.getDetails());
+// const person = new Person('Shakil Khan', 25);
+// console.log(person.getDetails());
 
-const stu = new Student('Borhan Khan', 24, 'A+');
-console.log(stu.getDetails());
-console.log(stu.getGrade());
+// const stu = new Student('Borhan Khan', 24, 'A+');
+// console.log(stu.getDetails());
+// console.log(stu.getGrade());
---
+++ Problems/problem_5.ts
@@ -0,0 +1,16 @@
+function logTheString(param: unknown): void {
+  if (
+    typeof param === 'string'
+    ) {
+
+    console.log(param);
+    
+  } 
+  
+  else {
+    console.error('Hey man please give me a string!!!');
+  }
+}
+
+logTheString('986');
+logTheString(986);
